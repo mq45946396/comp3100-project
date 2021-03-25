ccffd3f13dcffa15452f971357f330e36790c9a3
Jack Davenport <jack.davenport1@students.mq.edu.au>
2021-03-25 21:08:20 +1100

    Added socket I/O boilerplate

---
+++ Client.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.net.*;
+
+/**
+ *    DS-CLIENT IMPLEMENTATION
+ *    COMP3100, March 2021
+ *
+ *    by Jack Davenport (45946396)
+ *       Lucas Turnbull (????????)
+ *       Elliot Holt    (????????)
+ */
+public class Client {
+
+    static final String HOST = "localhost"; // the host address of the server
+    static final int    PORT = 50000;       // the TCP port of the server
+    static final String USER = "dsclient";  // the user name used for authentication
+
+    public static void main(String[] args) throws Exception {
+
+        // open socket
+	Socket socket = new Socket(HOST, PORT);
+	InputStream in = socket.getInputStream();
+	OutputStream out = socket.getOutputStream();
+
+	// begin handshake
+	send("HELO", out);
+
+	// TODO
+
+    }
+
+    /**
+     *	Write the command string into the given OutputStream.
+     *	@param str The command to send in the stream.
+     *	@param out The target stream to write the command to.
+     */
+    public void send(String str, OutputStream out) {
+	byte[] buf = str.getBytes();
+	out.write(buf);
+	out.flush();
+    }
+
+    /**
+     *	Reads the command string from the given InputStream.
+     *	This method will block until the entire string is received from the server.
+     *	@param in The stream to read the command string from.
+     *	@return The command string from the stream.
+     */
+    public String read(InputStream in) {
+        byte[] buf = new byte[1024];
+	int nBytes = in.read(buf);
+	return new String(buf, 0, nBytes);
+    }
+
+}
