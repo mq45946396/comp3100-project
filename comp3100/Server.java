6a150875375ab47fb5f669c0cf31d7d56ea560fd
Jack Davenport <jack.davenport1@students.mq.edu.au>
2021-04-16 23:48:56 +1000

    Added basic fields to Server.java

--- comp3100/Server.java
+++ comp3100/Server.java
@@ -2,6 +2,12 @@ package comp3100;
 
 public class Server {
 
-    // TODO
+    public final String serverType;
+    public final int serverID;
+    public final int core, mem, disk;
+    
+    // TODO: add these fields: state, curStartTime
+
+    // TODO: add constructor to set these fields
 
 }
