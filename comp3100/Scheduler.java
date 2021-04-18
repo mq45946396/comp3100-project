9cf3b50d5edb798c284f95d874f1b599818eb0d3
Jack Davenport <jack.davenport1@students.mq.edu.au>
2021-04-19 01:14:41 +1000

    Replaced getAllServers(), all jobs go to largest, small bug fixes

--- comp3100/Scheduler.java
+++ comp3100/Scheduler.java
@@ -1,11 +1,12 @@
 package comp3100;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Scheduler {
+
+    private static Server allTimeLargest = null;
 
     public static void scheduleJob(Connection conn, String[] args) throws Exception {
         // decode job parameters
@@ -26,10 +27,14 @@ public class Scheduler {
         if(bestServer != null) {
             String fullServerName = bestServer.type + " " + bestServer.id;
             conn.sendf("SCHD %d %s", jobID, fullServerName);
-            System.out.printf("[Job %d] Scheduled on %s (t = $d)\n", jobID, fullServerName, submitTime);
-        } else {
-            System.out.printf("[Job %d] Could not find server to schedule!", jobID);
+            if(!conn.read().equals("OK")) {
+                throw new RuntimeException("[Job " + jobID + "] Failed to schedule job!");
+            }
+            System.out.printf("[Job %d] Scheduled on %s (t = %d)\n", jobID, fullServerName, submitTime);
         }
+        // else {
+        //     System.out.printf("[Job %d] Could not find server to schedule!\n", jobID);
+        // }
 
         // indicate that we are ready for more work
         conn.send("REDY");
@@ -42,69 +47,40 @@ public class Scheduler {
      * @return Array of servers with their data.
      */
     private static Server[] getAllServers(Connection conn) {
-        /**
-         *  For Lucas to complete only
-         *
-         *  To ensure there's no possibility of merge conflicts, don't edit this method
-         *  unless you're Lucas and on Lucas' branch.
-         */
+        try {
+            // send GETS command
+            conn.send("GETS All");
 
-        boolean response = false;
-        boolean sent = false;
-        String input = "";
-        String[] moreInput = {};
+            // read data response
+            String[] data = conn.read().split(" ");
+            int numServers = Integer.parseInt(data[1]);
+            conn.send("OK");
 
-        while (!response) {
-            if (!sent) {
-                try {
-                    conn.sendf("GETS %s", "All");
-                    sent = true;
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                    return null;
-                }
+            Server[] servers = new Server[numServers];
+
+            // read server info
+            String[] info = conn.read().split("\n");
+            for(int i = 0; i < numServers; i++) {
+                // parse data
+                data = info[i].split(" ");
+                String type = data[0];
+                int id = Integer.parseInt(data[1]);
+                String state = data[2];
+                int cores = Integer.parseInt(data[4]);
+                int mem = Integer.parseInt(data[5]);
+                int disk = Integer.parseInt(data[6]);
+                // initialize server class
+                servers[i] = new Server(type, id, cores, mem, disk, 0, 0, 0, state, 0);
             }
-            try {
-                input = conn.read();
-                if (input.contains(" ")) {
-                    moreInput = input.substring(input.indexOf(" ")+1).split(" ");
-                    input = input.substring(0, input.indexOf(" "));
-                }
 
-                if (input.equals("DATA")) {
-                    conn.send("OK");
-                }
-                else {
-                    Server[] val = new Server[(moreInput.length+1)/9];
-                    val[0].type = input;
-                    val[0].id = Integer.valueOf(moreInput[0]);
-                    val[0].state = moreInput[1];
-                    val[0].curStartTime = Integer.valueOf(moreInput[2]);
-                    val[0].core = Integer.valueOf(moreInput[3]);
-                    val[0].mem = Integer.valueOf(moreInput[4]);
-                    val[0].disk = Integer.valueOf(moreInput[5]);
-                    if (val.length > 8) {
-                        int j = 1;
-                        for (int i = 7; i <= val.length-3; i+=9) {
-                            val[j].type = input;
-                            val[j].id = Integer.valueOf(moreInput[0]);
-                            val[j].state = moreInput[1];
-                            val[j].curStartTime = Integer.valueOf(moreInput[2]);
-                            val[j].core = Integer.valueOf(moreInput[3]);
-                            val[j].mem = Integer.valueOf(moreInput[4]);
-                            val[j].disk = Integer.valueOf(moreInput[5]);
-                            j+=1;
-                        }
-                    }
-                    return val;
-                }
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            conn.send("OK");
+            conn.read(); // reads the final '.' from server
+            return servers;
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
     /**
@@ -118,22 +94,18 @@ public class Scheduler {
      * @return The best available server to schedule the job on, or null if none can be found.
      */
     private static Server pickBestServer(Server[] servers, int jobCores, int jobMemory, int jobDisk) {
+        if(allTimeLargest != null) {
+            return allTimeLargest;
+        }
         List<Server> filtered = Arrays.asList(servers);
         filtered = Arrays.asList(filtered.stream().filter(s -> s.core >= jobCores).toArray(Server[]::new));
         filtered = Arrays.asList(filtered.stream().filter(s -> s.mem >= jobMemory).toArray(Server[]::new));
         filtered = Arrays.asList(filtered.stream().filter(s -> s.disk >= jobDisk).toArray(Server[]::new));
+        filtered = Arrays.asList(filtered.stream().filter(s -> !s.state.equals("unavailable")).toArray(Server[]::new));
+        Collections.sort(filtered);
         if (!filtered.isEmpty()) {
-            Collections.sort(filtered);
-            Server val = filtered.get(0);
-            if (!val.state.equals("inactive") && !val.state.equals("idle")) {
-                for (int i = 0; i < filtered.size(); i++) {
-                    if (val.state.equals("inactive") && val.state.equals("idle")) {
-                        val = filtered.get(i);
-                        return val;
-                    }
-                }
-            }
-            return null;
+            allTimeLargest = filtered.get(0);
+            return filtered.get(0);
         }
         else {
             return null;
