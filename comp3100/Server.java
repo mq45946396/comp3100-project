32d0811ff8a33bec5f8c50a9ec8bc93a38bb3aec
Jack Davenport <jack.davenport1@students.mq.edu.au>
2021-05-27 15:20:22 +1000

    Added stage 2 testing program

--- comp3100/Server.java
+++ comp3100/Server.java
@@ -34,36 +34,4 @@ public class Server implements Comparable<Server>{
         return s.core-this.core;
     }
 
-    // /* this method isn't needed */
-    // @Deprecated
-    // public static void main(String[] args) {
-    //     try {
-    //         ServerSocket serverSocket = new ServerSocket(Client.PORT);
-    //         Socket clientSocket = serverSocket.accept();
-
-    //         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    //         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-
-    //         boolean quit = false;
-    //         while(!quit) {
-    //             String input = in.readLine();
-    //             String output = "";
-
-    //             if (input.equals("HELO") || input.equals("AUTH " + Client.USER)) {
-    //                 out.write("OK");
-    //                 output = "OK";
-    //             }
-    //             if (input.equals("REDY")) {
-    //                 quit = true;
-    //             }
-    //             System.out.println("I receive: " + input);
-    //             System.out.println("You receive: " + output);
-    //         }
-
-    //         out.close();
-    //         serverSocket.close();
-    //         clientSocket.close();
-    //     } catch (Exception e){System.out.println(e);}
-    // }
-
 }
